53fdaee9a313d557bec93e39a976d34d286f599f
macbookpro <daewu.bintara1996@gmail.com>
2021-12-17 21:40:52 +0700

    ServerTest, HandlerTest

---
+++ handler_test.go
@@ -0,0 +1,22 @@
+package belajar_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello world")
+	}
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
