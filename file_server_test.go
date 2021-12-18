2be9b2e98604c17aa29f88521f27711cc921166d
macbookpro <daewu.bintara1996@gmail.com>
2021-12-18 10:41:20 +0700

    TestFileServerEmbed

--- file_server_test.go
+++ file_server_test.go
@@ -1,6 +1,8 @@
 package belajar_web
 
 import (
+	"embed"
+	"io/fs"
 	"net/http"
 	"testing"
 )
@@ -17,6 +19,28 @@ func TestFileServer(t *testing.T) {
 	}
 
 	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources
+var resources embed.FS
+func TestFileServerEmbed(t *testing.T) {
+	directory, err := fs.Sub(resources,"resources")
+	if err != nil {
+		panic(err)
+	}
+	fileServer := http.FileServer(http.FS(directory))
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err = server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
